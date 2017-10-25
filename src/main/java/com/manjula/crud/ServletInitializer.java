5d0fddef9a9aae983a13893d09edd9740ca875c1
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-10-25 10:48:31 +0530

    refactor configurations

--- src/main/java/com/manjula/crud/ServletInitializer.java
+++ src/main/java/com/manjula/crud/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.manjula.crud.config;
+package com.manjula.crud;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
